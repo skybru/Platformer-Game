30c6a0d5de44f3a6caa775f64cb6df734035bcb2
Jojo <giorgio.bruschi.ext@luxottica.com>
2024-08-27 13:19:20 +0200

    implemented Player's class

--- script.js
+++ script.js
@@ -13,3 +13,55 @@ const proportionalSize = (size => {
     return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
 });
 
+class Player {
+    constructor() {
+        this.position = {
+            x: proportionalSize(10),
+            y: proportionalSize(400)
+        };
+
+        this.velocity = {
+            x: 0,
+            y: 0
+        };
+
+        this.width = proportionalSize(40);
+        this.height = proportionalSize(40);
+    }
+
+    draw() {
+        ctx.fillStyle = "#99c9ff";
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+            if (this.position.y < 0) {
+                this.position.y = 0;
+                this.velocity.y = gravity;
+            }
+
+            this.velocity.y += gravity;
+        } else {
+            this.velocity.y = 0;
+        }
+
+        if (this.position.x < this.width) {
+            this.position.x = this.width;
+        }
+        //setting the player x position to max value so it doesn't fall off the right edge
+        if (this.position.x >= canvas.width - this.width * 2) {
+            this.position.x = canvas.width - this.width * 2;
+        }
+    }
+};
+
+const player = new Player();
+const startGame = () => {
+    canvas.style.display = "block";
+    startScreen.style.display = "none";
+};
