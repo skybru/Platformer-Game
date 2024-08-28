80b73af32d34e3b9466711ed2fdfaf4f32224bdb
Jojo <giorgio.bruschi.ext@luxottica.com>
2024-08-28 10:37:00 +0200

    added collision

--- script.js
+++ script.js
@@ -116,7 +116,9 @@ const animate = () => {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     platforms.forEach((platform) => platform.draw());
+
     player.update();
+
     //using proportionalSize() to make sure player's x is proportional to screen size
     if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
         player.velocity.x = 5;
@@ -126,6 +128,41 @@ const animate = () => {
         player.velocity.x = 0;
     }
 
+    if (keys.rightKey.pressed && isCheckpointCollisionDetectionActive) {
+        platforms.forEach((platform) => {
+            platform.position.x -= 5;
+        });
+    } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
+        platforms.forEach((platform) => {
+            platform.position.x += 5;
+        });
+    }
+
+    platforms.forEach(platform => {
+        const collisionDetectionRules = [
+            player.position.y + player.height <= platform.position.y, //collision under platform
+            player.position.y + player.height + player.velocity.y >= platform.position.y, //collision above platform
+            player.position.x >= platform.position.x - player.width / 2, //collision right
+            player.position.x <= platform.position.x + platform.width - player.width / 3 //collision left
+        ];
+
+        if (collisionDetectionRules.every((rule) => rule)) {
+            player.velocity.y = 0;
+            return;
+        }
+
+        const platformDetectionRules = [
+            player.position.x >= platform.position.x - player.width / 2,
+            player.position.x <= platform.position.x + platform.width - player.width / 3,
+            player.position.y + player.height >= platform.position.y,
+            player.position.y <= platform.position.y + platform.height,
+        ];
+
+        if (platformDetectionRules.every((rule) => rule)) {
+            player.position.y = platform.position.y + player.height;
+            player.velocity.y = gravity;
+        }
+    });
 };
 
 const movePlayer = (key, xVelocity, isPressed) => {
